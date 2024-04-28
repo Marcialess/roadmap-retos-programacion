71eafc7b2328e86612a5521408de8c1ee83a2649
Marcial Abad <108768099+Marcialess@users.noreply.github.com>
2024-04-28 17:04:58 -0400

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Marcialess.py
@@ -0,0 +1,23 @@
+# https://python.org/
+
+# Comentario de una linea
+
+"""
+Ejemplo
+Comentario de varias lineas
+"""
+
+my_first_var = "Esta es mi primera variable"
+
+# Esto es una constante en Python se coloca en mayuscula por convencion.
+MY_FIRST_CONST = 162024
+
+var_str = "Hola Mundo"
+var_int = 354
+var_float = 23.5
+var_bool_one = True
+var_bool_two = False 
+
+my_language = "Python"
+
+print(f'Hola, {my_language}')
